cf3b040286153ed1f1636469600ca5909e0f646c
Edward B. Smith <smith.ed.b@gmail.com>
2015-01-31 11:38:54 -0800

    Duh.

--- psql/psql.go
+++ psql/psql.go
@@ -7,36 +7,37 @@ package psql
 
 
 import (
-	"io"
-	"sync"
-	"bufio"
-	"errors"
-	"strings"
-	"strconv"
-	"os/exec"
-	"net/url"
-	"database/sql"
-	_ "github.com/lib/pq"
-	"violent.blue/go/log"
-	)
+    "io"
+    "fmt"
+    "sync"
+    "bufio"
+    "errors"
+    "strings"
+    "strconv"
+    "os/exec"
+    "net/url"
+    "database/sql"
+    _ "github.com/lib/pq"
+    "violent.blue/go/log"
+    )
 
 
-var PGCTLPath string 	= ""
-var PSQLPath string 	= ""
+var PGCTLPath string    = ""
+var PSQLPath string     = ""
 var PSQLDataPath string = ""
-var DB *sql.DB			= nil
+var DB *sql.DB          = nil
 
-var Name		= ""
+var Name        = ""
 var Host        = "localhost"
-var User		= "postgres"
-var Password	= ""
-var Port		= 5432
+var User        = "postgres"
+var Password    = ""
+var Port        = 5432
 
 
 func ConnectDatabase(databaseURI string) error {
-	//
-	//	Start the database --
-	//
+    //
+    //  Start the database --
+    //
 
     if databaseURI != "" {
         u, error := url.Parse(databaseURI)
@@ -50,9 +51,9 @@ func ConnectDatabase(databaseURI string) error {
 
         if u.Scheme == "db" || u.Scheme == "psql" || u.Scheme == "sql" {
         } else {
-        	log.Error("Invalid database scheme '%s'", u.Scheme)
-        	return errors.New("Invalid scheme")
-        	}
+            log.Error("Invalid database scheme '%s'", u.Scheme)
+            return errors.New("Invalid scheme")
+            }
 
         i := strings.IndexRune(u.Host, ':')
         if i >= 0 {
@@ -72,126 +73,144 @@ func ConnectDatabase(databaseURI string) error {
         log.Debug("Host: %s Port: %d User: %s Pass: %s Name: %s.", Host, Port, User, Password, Name)
         }
 
-	//	Find postgres --
+    //  Find postgres --
 
-	var error error
-	PGCTLPath, error = exec.LookPath("pg_ctl")
-	if error != nil {
-		log.Error("Can't find Postgres 'pg_ctl': %v.", error)
-		return error
-		}
-	log.Debug(" Found pg_ctl: %v.", PGCTLPath)
-	log.Debug("Database Data: %v.", PSQLDataPath)
+    var error error
+    PGCTLPath, error = exec.LookPath("pg_ctl")
+    if error != nil {
+        log.Error("Can't find Postgres 'pg_ctl': %v.", error)
+        return error
+        }
+    log.Debug("   Found pg_ctl: %v.", PGCTLPath)
+    log.Debug("Using data path: %v.", PSQLDataPath)
 
-	command := exec.Command(PGCTLPath, "status", "-D", 	PSQLDataPath)
-	error = command.Run()
-	if command.ProcessState.Sys() == 3 {
-		log.Debug("Starting Postgres")
-		command = exec.Command(PGCTLPath, "start", "-w", "-s", "-D", PSQLDataPath)
-		error = command.Run()
-		if error != nil {
-			log.Error("Can't start Postgress: %v.", error)
-			return error
-			}
-	} else {
-		log.Debug("Postgres is already started.")
-		}
-
-
-	//
-	//	Find psql command line utility and connect --
-	//
+//  command := exec.Command(PGCTLPath, "status", "-D",  PSQLDataPath)
+    command := exec.Command(PGCTLPath, "status")
+    error = command.Run()
+    if command.ProcessState.Sys() == 3 {
+        log.Debug("Starting Postgres")
+//      command = exec.Command(PGCTLPath, "start", "-w", "-s", "-D", PSQLDataPath)
+        command = exec.Command(PGCTLPath, "start", "-w", "-s")
+        error = command.Run()
+        if error != nil {
+            log.Error("Can't start Postgress: %v.", error)
+            return error
+            }
+    } else {
+        log.Debug("Postgres is already started.")
+    }
 
 
-	//	Find psql --
-
-	PSQLPath, error = exec.LookPath("psql")
-	if error != nil {
-		log.Error("Can't find Postgres 'psql': %v.", error)
-		return error
-		}
-	log.Debug("psqlpath: %v.", PSQLPath)
-
-	//	Make a connection --
+    //
+    //  Find psql command line utility and connect --
+    //
 
 
+    //  Find psql --
 
-	DB, error = sql.Open("postgres", "user=Edward dbname=Edward sslmode=disable")
-	if error != nil {
-		DB = nil
-		log.Error("Error: Can't open database connection: %v.", error);
-		return error
-		}
+    PSQLPath, error = exec.LookPath("psql")
+    if error != nil {
+        log.Error("Can't find Postgres 'psql': %v.", error)
+        return error
+        }
+    log.Debug("psqlpath: %v.", PSQLPath)
 
-	return nil
-	}
+    //  Make a connection --
+
+    connectString :=
+        fmt.Sprintf("host=%s port=%d  dbname=%s user=%s password=%s sslmode=disable",
+                     Host, Port, Name, User, Password)
+    log.Debug("Connection string: %s.", connectString)
+    DB, error = sql.Open("postgres", connectString)
+    if error != nil {
+        DB = nil
+        log.Error("Error: Can't open database connection: %v.", error);
+        return error
+        }
+
+    //  Get our settings --
+    //  select setting from pg_settings where name = 'port';
+    rows, error := DB.Query("select current_user, inet_server_addr(), inet_server_port(), current_database(), current_schema;")
+    if error != nil {
+        log.Error("Error querying database config: %v.", error)
+    } else {
+        defer rows.Close()
+        var (user string; host string; port int; database string; schema string)
+        for rows.Next() {
+            rows.Scan(&user, &host, &port, &database, &schema)
+            log.Debug("Connected to database psql:%s@%s:%d/%s (%s).", user, host, port, database, schema)
+        }
+    }
+
+    return nil
+    }
 
 
 func DisconnectDatabase() {
-	if  DB != nil {
-		DB.Close()
-		DB = nil
-		Host = "localhost"
-		Port = 5432
-		Name = "postgres"
-		User = "postgres"
-		}
-	}
+    if  DB != nil {
+        DB.Close()
+        DB = nil
+        Host = "localhost"
+        Port = 5432
+        Name = "postgres"
+        User = "postgres"
+        }
+    }
 
 
 func RunScript(script string) error {
 
-	//
-	//	Run an SQL script that is stored as a resource --
-	//
+    //
+    //  Run an SQL script that is stored as a resource --
+    //
 
-	var error error
-	psqlOptions := [] string {
-		"-h", "localhost",
-		"-X", "-q",
-		"-v", "ON_ERROR_STOP=1",
-		"--pset", "pager=off",
-		}
-	command := exec.Command(PSQLPath, psqlOptions...)
-	command.Env = append(command.Env, "PGOPTIONS=-c client_min_messages=WARNING")
-	commandpipe, error := command.StdinPipe()
-	if error != nil {
-		log.Error("Can't open pipe: %v", error)
-		return error
-		}
+    var error error
+    psqlOptions := [] string {
+        "-h", "localhost",
+        "-X", "-q",
+        "-v", "ON_ERROR_STOP=1",
+        "--pset", "pager=off",
+        }
+    command := exec.Command(PSQLPath, psqlOptions...)
+    command.Env = append(command.Env, "PGOPTIONS=-c client_min_messages=WARNING")
+    commandpipe, error := command.StdinPipe()
+    if error != nil {
+        log.Error("Can't open pipe: %v", error)
+        return error
+        }
 
-	var errorpipe *io.PipeReader;
-	errorpipe, command.Stderr = io.Pipe()
+    var errorpipe *io.PipeReader;
+    errorpipe, command.Stderr = io.Pipe()
 
-	error = command.Start()
-	if error != nil {
-		log.Error("Error running psql: %v.", error)
-		return error
-		}
+    error = command.Start()
+    if error != nil {
+        log.Error("Error running psql: %v.", error)
+        return error
+        }
 
-	commandpipe.Write([]byte(script))
-	commandpipe.Close()
+    commandpipe.Write([]byte(script))
+    commandpipe.Close()
 
-	var waiter sync.WaitGroup
-	waiter.Add(1)
-	go func() {
-		scanner := bufio.NewScanner(errorpipe)
-		for scanner.Scan() {
-			log.Error("%v.", scanner.Text())
-			}
-		waiter.Done()
-		} ()
+    var waiter sync.WaitGroup
+    waiter.Add(1)
+    go func() {
+        scanner := bufio.NewScanner(errorpipe)
+        for scanner.Scan() {
+            log.Error("%v.", scanner.Text())
+            }
+        waiter.Done()
+        } ()
 
-	error = command.Wait()
-	errorpipe.Close()
-	waiter.Wait()
+    error = command.Wait()
+    errorpipe.Close()
+    waiter.Wait()
 
-	if error != nil {
-		log.Error("Script %v.", error)
-		return error
-		}
+    if error != nil {
+        log.Error("Script %v.", error)
+        return error
+        }
 
-	return nil
-	}
+    return nil
+    }
 
 
